cdb6681bc2009a0114111f252e159ccca2bd74fa
Joshua Clayton <joshua.clayton@gmail.com>
2017-04-04 16:58:07 -0400

    Extract todo creation to user-scoped blocks

--- spec/features/user_views_own_todos_spec.rb
+++ spec/features/user_views_own_todos_spec.rb
@@ -2,13 +2,25 @@ require "rails_helper"
 
 RSpec.feature "User views own todos" do
   scenario "successfully" do
-    sign_in_as "person@example.com"
-    create_todo "Buy eggs"
+    as_user "person@example.com" do
+      create_todo "Buy eggs"
+      create_todo "Buy cheese"
+      create_todo "Buy broccoli"
+    end
 
-    sign_in_as "different@example.com"
-    create_todo "Buy milk"
+    as_user "different@example.com" do
+      create_todo "Buy milk"
 
-    expect(page).to have_todo("Buy milk")
-    expect(page).not_to have_todo("Buy eggs")
+      expect(page).to have_todo("Buy milk")
+      expect(page).not_to have_todo("Buy eggs")
+    end
+  end
+
+  def as_user(email, &block)
+    sign_in_as email
+
+    block.call
+
+    sign_in_as ""
   end
 end
